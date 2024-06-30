a62c7443cfc9858bf6e112067ad5704ae76c798e
Jan <jan810693@gmail.com>
2024-06-30 18:09:37 +0530

    sound + siri wave added

--- front/main.js
+++ front/main.js
@@ -10,4 +10,33 @@ $(document).ready(function () {
             effect: "bounceOut",
         },
     });
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+    // Siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
+    // Mic button click event
+    $("#MicBtn").click(function () { 
+        eel.playassistantsound()
+        $("#Oval").addClass("hidden");
+        $("#SiriWave").removeClass("hidden");
+    });
 });
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,11 +2,8 @@
 import os
 import eel
 
-from engine.features import *
-
 eel.init("front")
-
-playassistantsound()
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html', mode='None', host='localhost', block=True)
 
+#jansiddiqui
