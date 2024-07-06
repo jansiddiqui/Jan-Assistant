2ead746937145d2f2ebc1696a8428e1784632510
Jan <jan810693@gmail.com>
2024-07-06 06:55:28 +0530

    Whatsapp Features

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 conn = sqlite3.connect("jan.db")
@@ -10,12 +11,12 @@ cursor.execute(query)
 # cursor.execute(query)
 # conn.commit()
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
 
-query = r"INSERT INTO web_command VALUES(null,'geeksforgeeks', 'https://www.geeksforgeeks.org//')"
-cursor.execute(query)
-conn.commit()
+# query = r"INSERT INTO web_command VALUES(null,'geeksforgeeks', 'https://www.geeksforgeeks.org//')"
+# cursor.execute(query)
+# conn.commit()
 
 
 # # Testing Module
@@ -23,3 +24,30 @@ conn.commit()
 # cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name))
 # results = cursor.fetchall()
 # print(results)
+
+#create table with desired column
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+# # Specify the column indices you want to import (0-based index)
+# desired_column_indices = [0, 33]
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_column_indices]
+#         cursor.execute('''INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+# conn.commit()
+# conn.close()
+
+# # Insert Single contacts (Optional)
+# query = "INSERT INTO contacts VALUES (null,'Lakhan', '6394343032', 'null')"
+# cursor.execute(query)
+# conn.commit()
+
+# Search Contacts from database
+query = 'Diksha Tripathi'
+query = query.strip().lower()
+
+cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+results = cursor.fetchall()
+print(results[0][0])
