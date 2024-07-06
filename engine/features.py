7503072de3be5b1a1dc087fcc63b9d4c3a918e30
Jan <jan810693@gmail.com>
2024-07-06 22:02:40 +0530

    whatsapp call and message features

--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,7 @@
 #features.py
+from pipes import quote
 import struct
+import subprocess
 import time
 from playsound import playsound
 import eel
@@ -9,10 +11,11 @@ import sqlite3
 import webbrowser
 import pvporcupine
 import pyaudio
+import pyautogui
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
-from engine.helper import extract_yt_term
+from engine.helper import extract_yt_term, remove_words
 
 conn = sqlite3.connect("jan.db")
 cursor = conn.cursor()
@@ -92,3 +95,61 @@ def hotword():
             audio_stream.close()
         if paud is not None:
             paud.terminate()
+
+# Find Contacts
+def findContact(query):
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'wahtsapp', 'video']
+    query = remove_words(query, words_to_remove)
+
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results = cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str = str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str = '+91' + mobile_number_str
+
+        return mobile_number_str, query
+    except:
+        speak('not exist in contacts')
+        return 0, 0
+
+
+def whatsApp(mobile_no, message, flag, name):
+
+    if flag == 'message':
+        target_tab = 13
+        jan_message = "message send successfully to "+name
+
+    elif flag == 'call':
+        target_tab = 7
+        message = ''
+        jan_message = "calling to "+name
+
+    else:
+        target_tab = 6
+        message = ''
+        jan_message = "staring video call with "+name
+
+    # Encode the message for URL
+    encoded_message = quote(message)
+
+    # Construct the URL
+    whatsapp_url = f"whatsapp://send?phone={mobile_no}&text={encoded_message}"
+
+    # Construct the full command
+    full_command = f'start "" "{whatsapp_url}"'
+
+    # Open WhatsApp with the constructed URL using cmd.exe
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+    subprocess.run(full_command, shell=True)
+    
+    pyautogui.hotkey('ctrl', 'f')
+
+    for i in range(1, target_tab):
+        pyautogui.hotkey('tab')
+
+    pyautogui.hotkey('enter')
+    speak(jan_message)
